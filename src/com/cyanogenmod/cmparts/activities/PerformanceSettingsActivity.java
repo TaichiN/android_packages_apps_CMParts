3619fafc41b7ac0aa17bf714593e0927d3030e78
fuzz <fuzzzzzzzzzzzzzzzzzz@gmail.com>
2013-07-07 12:00:05 +0900

    Revert "Performance settings: Refactoring"

--- src/com/cyanogenmod/cmparts/activities/PerformanceSettingsActivity.java
+++ src/com/cyanogenmod/cmparts/activities/PerformanceSettingsActivity.java
@@ -26,6 +26,9 @@ import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
 import android.provider.Settings;
 
 import java.io.File;
@@ -35,6 +38,23 @@ import java.io.File;
  */
 public class PerformanceSettingsActivity extends PreferenceActivity {
 
+    private static final String JIT_PREF = "pref_jit_mode";
+    private static final String JIT_ENABLED = "int:jit";
+    private static final String JIT_DISABLED = "int:fast";
+    private static final String JIT_PERSIST_PROP = "persist.sys.jit-mode";
+    private static final String JIT_PROP = "dalvik.vm.execution-mode";
+    private static final String USE_DITHERING_PREF = "pref_use_dithering";
+    private static final String USE_DITHERING_PERSIST_PROP = "persist.sys.use_dithering";
+    private static final String USE_DITHERING_DEFAULT = "1";
+    private static final String USE_16BPP_ALPHA_PREF = "pref_use_16bpp_alpha";
+    private static final String USE_16BPP_ALPHA_PROP = "persist.sys.use_16bpp_alpha";
+    private static final String DISABLE_BOOTANIMATION_PREF = "pref_disable_bootanimation";
+    private static final String DISABLE_BOOTANIMATION_PERSIST_PROP = "persist.sys.nobootanimation";
+    private static final String DISABLE_BOOTANIMATION_DEFAULT = "0";
+    private CheckBoxPreference mJitPref;
+    private CheckBoxPreference mUseDitheringPref;
+    private CheckBoxPreference mUse16bppAlphaPref;
+    private CheckBoxPreference mDisableBootanimPref;
     private AlertDialog alertDialog;
 
     @Override
@@ -43,6 +63,25 @@ public class PerformanceSettingsActivity extends PreferenceActivity {
 
         setTitle(R.string.performance_settings_title_subhead);
         addPreferencesFromResource(R.xml.performance_settings);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mJitPref = (CheckBoxPreference) prefSet.findPreference(JIT_PREF);
+        String jitMode = SystemProperties.get(JIT_PERSIST_PROP,
+                SystemProperties.get(JIT_PROP, JIT_ENABLED));
+        mJitPref.setChecked(JIT_ENABLED.equals(jitMode));
+
+        mUseDitheringPref = (CheckBoxPreference) prefSet.findPreference(USE_DITHERING_PREF);
+        String useDithering = SystemProperties.get(USE_DITHERING_PERSIST_PROP, USE_DITHERING_DEFAULT);
+        mUseDitheringPref.setChecked("1".equals(useDithering));
+
+        mUse16bppAlphaPref = (CheckBoxPreference) prefSet.findPreference(USE_16BPP_ALPHA_PREF);
+        String use16bppAlpha = SystemProperties.get(USE_16BPP_ALPHA_PROP, "0");
+        mUse16bppAlphaPref.setChecked("1".equals(use16bppAlpha));
+
+        mDisableBootanimPref = (CheckBoxPreference) prefSet.findPreference(DISABLE_BOOTANIMATION_PREF);
+        String disableBootanimation = SystemProperties.get(DISABLE_BOOTANIMATION_PERSIST_PROP, DISABLE_BOOTANIMATION_DEFAULT);
+        mDisableBootanimPref.setChecked("1".equals(disableBootanimation));
 
         // Set up the warning
         alertDialog = new AlertDialog.Builder(this).create();
@@ -59,4 +98,32 @@ public class PerformanceSettingsActivity extends PreferenceActivity {
         alertDialog.show();
     }
 
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mJitPref) {
+            SystemProperties.set(JIT_PERSIST_PROP,
+                    mJitPref.isChecked() ? JIT_ENABLED : JIT_DISABLED);
+            return true;
+        }
+
+        if (preference == mUseDitheringPref) {
+            SystemProperties.set(USE_DITHERING_PERSIST_PROP,
+                    mUseDitheringPref.isChecked() ? "1" : "0");
+            return true;
+        }
+
+        if (preference == mUse16bppAlphaPref) {
+            SystemProperties.set(USE_16BPP_ALPHA_PROP,
+                    mUse16bppAlphaPref.isChecked() ? "1" : "0");
+            return true;
+        }
+
+        if (preference == mDisableBootanimPref) {
+            SystemProperties.set(DISABLE_BOOTANIMATION_PERSIST_PROP,
+                    mDisableBootanimPref.isChecked() ? "1" : "0");
+            return true;
+        }
+
+        return false;
+    }
 }
